003381e3e4cfdb8e44013f434df0caea8eecda63
DAVIDOWSKI ENGENEERING <davidoski@pc55.home>
2014-05-07 11:04:10 +0200

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+	  watch: {
+		all: {
+		  options: { livereload: true, spawn: false },
+		  files: ['source/**/*']
+		}
+	  },
+
+	  karma: {
+		unit: {
+			configFile: 'karma.conf.js'/*,
+			background: true*/
+		}
+	  }
+
+	});
+
+}
