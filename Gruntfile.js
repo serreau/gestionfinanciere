a78d6bb7bf9633b3fd93d8231edd71b3a7600777
David Miorin <davidoski@MacBook-Pro-de-DAVIDOSKI.local>
2014-05-09 13:39:16 +0200

    Fixing Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,38 +6,38 @@ module.exports = function(grunt) {
 
 		/* AUTOMATISATION */
 		watch: {
-		//all: {
-		//  options: { livereload: true, spawn: false },
-		//  files: ['source/**/*']
-		//},
-		html: {
-		    files: ['src/**/*.html'],
-		    tasks: ['copy'],
-		    options: {
-		        livereload: true
-		    }
-		},
-		img: {
-		    files: ['src/img/**/*.png', 'src/img/**/*.jpg', 'src/img/**/*.gif'],
-		    tasks: ['imagemin.dev'],
-		    options: {
-		        livereload: true
-		    }
-		},
-		js: {
-		    files: ['src/**/*.js'],
-		    tasks: ['jshint', 'concat.dev', 'uglify.dev'],
-		    options: {
-		        livereload: true
-		    }
-		},
-		scss: {
-		    files: ['src/**/*.scss'],
-		    tasks: ['sass.dev', 'csslint'],
-		    options: {
-		        livereload: true
-		    }
-		}
+    		//all: {
+    		//  options: { livereload: true, spawn: false },
+    		//  files: ['source/**/*']
+    		//},
+    		html: {
+    		    files: ['src/**/*.html'],
+    		    tasks: ['copy'],
+    		    options: {
+    		        livereload: true
+    		    }
+    		},
+    		img: {
+    		    files: ['src/img/**/*.png', 'src/img/**/*.jpg', 'src/img/**/*.gif'],
+    		    tasks: ['imagemin:dev'],
+    		    options: {
+    		        livereload: true
+    		    }
+    		},
+    		js: {
+    		    files: ['src/**/*.js'],
+    		    tasks: ['jshint'/*, 'concat.dev'*/, 'uglify:dev'],
+    		    options: {
+    		        livereload: true
+    		    }
+    		},
+    		scss: {
+    		    files: ['src/**/*.scss'],
+    		    tasks: ['sass:dev'/*, 'csslint'*/],
+    		    options: {
+    		        livereload: true
+    		    }
+    		}
 		},
 
 		karma: {
@@ -50,14 +50,13 @@ module.exports = function(grunt) {
 		/* TRANSFORMATION DU CODE */
 		sass: {
 			dev: {
-			    options: {
-			        style: 'nested',
-			        noCache: false,
-			        banner: '/*! <%= pkg.appname %> version <%= pkg.appversion %> */\n',
-			    },
-			    files: {
-			        'src/<%= pkg.appname %>.scss': ['dev/css/<%= pkg.appname %>.css']
-			    }
+                options: {                       // Target options
+                    style: 'expanded'
+                },
+                    files: {                         // Dictionary of files
+                    'www/css/style.css': 'src/scss/style.scss'//,       // 'destination': 'source'
+                    //'widgets.css': 'widgets.scss'
+                }
 			},
 			build: {
 			    options: {
@@ -76,47 +75,69 @@ module.exports = function(grunt) {
 			src: ['src/**/*.js']
 		},
 		csslint: {
-			options: {
-			    import: 2
-			},
-			src: ['dev/css/<%= pkg.appname %>.css']
-		}
-
-      	/* MISE EN FORME DES FICHIERS */
-        concat: {
-            options: {
-                stripBanners: true,
-                banner: '/*! <%= pkg.appname %> version <%= pkg.appversion %> concatened */\n',
-            },
             dev: {
-                src: ['src/**/*.js'],
-                dest: 'dev/js/<%= pkg.appname %>.js'
+                options: {
+                    import: 2
+                },
+                src: ['www/css/style.css']
             },
             build: {
-                src: ['src/**/*.js'],
-                dest: 'build/js/<%= pkg.appname %>.js'
+                options: {
+                    import: 2
+                },
+                src: ['build/css/style.css']
             }
-        },
+		},
+
+      	/* MISE EN FORME DES FICHIERS */
+        //concat: {
+        //    options: {
+        //        stripBanners: true,
+        //        banner: '/*! <%= pkg.appname %> version <%= pkg.appversion %> concatened */\n',
+        //    },
+        //    dev: {
+        //        src: ['src/**/*.js'],
+        //        dest: 'dev/js/<%= pkg.appname %>.js'
+        //    },
+        //    build: {
+        //        src: ['src/**/*.js'],
+        //        dest: 'build/js/<%= pkg.appname %>.js'
+        //    }
+        //},
         uglify: {
             dev: {
                 options: {
+                    mangle: false,
                     compress: false,
-                    beautify: true,
+                    beautify: true
                 },
                 files: {
-                    'dev/js/<%= pkg.appname %>.min.js': ['dev/js/<%= pkg.appname %>.js']
+                    /*'dev/js/<%= pkg.appname %>.min.js': ['dev/js/<%= pkg.appname %>.js']*/
+                    'www/js/output.min.js': ['src/**/*.js']
                 }
             },
             build: {
                 options: {
+                    mangle: false,
                     compress: true,
-                    beautify: false,
+                    beautify: false
                 },
                 files: {
-                    'build/js/<%= pkg.appname %>.min.js': ['build/js/<%= pkg.appname %>.js']
+                    //'build/js/<%= pkg.appname %>.min.js': ['build/js/<%= pkg.appname %>.js']
+                    'build/js/output.min.js': ['src/input.js']
                 }
             }
         },
+        /*uglify: {
+            options: {
+              mangle: false
+            },
+            my_target: {
+              files: {
+                'dest/output.min.js': ['src/input.js']
+              }
+            }
+        },*/
         autoprefixer: {
             dev: {
                 src: 'dev/css/<%= pkg.appname %>.css',
@@ -127,46 +148,55 @@ module.exports = function(grunt) {
         },
         imagemin: {
             dev: {
-                options: {
-                    optimizationLevel: 1,
-                    progressive: false,
-                    interlaced: false
-                },
-                files: {
+                files: [{
                     expand: true,
                     cwd: 'src/img/',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: 'dev/img/'
-                }
+                    src: ['*.{png,jpg,gif}'],
+                    dest: 'www/img/'
+                }]
             },
             build: {
-                options: {
-                    optimizationLevel: 7
-                    progressive: true,
-                    interlaced: false
-                },
-                files: {
+                dynamic: {
+                  files: [{
                     expand: true,
-                    cwd: 'src/img/',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: 'build/img/'
+                    cwd: '/src/img/',
+                    src: ['*.{png,jpg,gif}'],
+                    dest: '/www/img/'
+                  }]
                 }
             }
         },
         copy: {
             dev : {
                 files: [
-                     {expand: true, src: ['src/**/*.html'], dest: 'dev/'},
-                     {expand: true, src: ['libs'], dest: 'dev/'}
+                     {expand: true, src: ['src/**/*.html'], flatten: true, dest: 'www/'}/*,
+                     {expand: true, src: ['libs'], dest: 'dev/'}*/
                 ]
             },
             build: {
                 files: [
-                     {expand: true, src: ['src/**/*.html'], dest: 'build/'},
-                     {expand: true, src: ['libs'], dest: 'build/'}
+                     {expand: true, src: ['src/**/*.html'], flatten: true, dest: 'build/'}/*,
+                     {expand: true, src: ['libs'], dest: 'build/'}*/
                 ]
             }
         },
+
+/*main: {
+    files: [
+      // includes files within path
+      {expand: true, src: ['path/*'], dest: 'dest/', filter: 'isFile'},
+
+      // includes files within path and its sub-directories
+      {expand: true, src: ['path/**'], dest: 'dest/'},
+
+      // makes all src relative to cwd
+      {expand: true, cwd: 'path/', src: ['**'], dest: 'dest/'},
+
+      // flattens results to a single level
+      {expand: true, flatten: true, src: ['path/**'], dest: 'dest/', filter: 'isFile'}
+    ]
+  } */
+
         compress: {
             build: {
                 options: {
@@ -196,16 +226,16 @@ module.exports = function(grunt) {
 				dest: '/path/to/destination/folder',
 				exclusions: ['build/**/.DS_Store', 'build/**/Thumbs.db', 'build/**/*.git*']
 			}
-		}
+		},
 
         /* NETTOYAGE */
-        clean: ['build/js/<%= pkg.appname %>.min.js', 'build/js/<%= pkg.appname %>.js', 'build/css/<%= pkg.appname %>.css', 'build/img/']
+        clean: ['build/js', 'build/css', 'build/img']
 
 	});
 
 	grunt.registerTask('default', ['watch']);
-    grunt.registerTask('dev', ['watch', 'sass', 'jshint', 'csslint', 'concat', 'autoprefixer', 'uglify', 'imagemin']);
-    grunt.registerTask('build', ['sass', 'concat', 'autoprefixer', 'uglify', 'imagemin', 'compress','ftp-deploy']);
+    grunt.registerTask('dev', ['watch', 'sass', 'jshint', /*'csslint',*/ /*'concat',*/ 'autoprefixer', 'uglify', 'imagemin']);
+    grunt.registerTask('build', ['sass', /*'concat', */'autoprefixer', 'uglify', 'imagemin', 'compress','ftp-deploy']);
     grunt.registerTask('clean', ['clean']);
 
 }
@@ -214,6 +244,8 @@ module.exports = function(grunt) {
 
 	TODO
 	----
+    files deleting
+    livereload deleting in build mode
     karma tests
 	start a http server
 	launch chrome in a particular URL
